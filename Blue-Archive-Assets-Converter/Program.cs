8ab0966ef381d1ad77b8583a3ba38787f92422af
Endergreen12 <endergreenjapan@gmail.com>
2023-05-07 00:42:43 +0900

    プロジェクト ファイルを追加します。

---
+++ Blue-Archive-Assets-Converter/Program.cs
@@ -0,0 +1,113 @@
+﻿// 最初にMediaPatchフォルダーをユーザーに指定させ、コピー先フォルダも指定させる。
+// pathArrayとcrcArrayを取得する。
+// forでcrcArrayを使ってファイルの存在を確認し、もし存在する場合はpathArrayからpathを取得し、"/"で文字列を分割し、最後のファイル名を除いたディレクトリを作成し、
+// そこにソースファイルをコピーし、名前を分割した文字列の最後の文字列に変更する。
+// もしそこにファイルが既に存在していた場合は、一致するか確認し、しない場合は削除してコピーしなおす
+// TODO: もっといい方法を見つける
+
+using static Blue_Archive_Assets_Converter.Utils;
+
+UpdateConsoleTitle(Status.Init);
+
+Console.WriteLine("{0}を含むディレクトリのフルパスを指定してください。\nおそらくMediaPatchという名前のフォルダだと思います", mediaCatalogJson);
+string? path = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("保存先のフォルダのフルパスを指定してください。");
+string? destPath = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("変換したいフォルダを指定してください。\n例: Audio/Cafe/Scenario/Video\nもしすべてのフォルダを変換したい場合は空欄にしてください。");
+string? folderName = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("ログを表示しますか？表示すると処理速度が低下します。\n速度を早くしたい場合は表示しないことをお勧めします。表示しなくてもタイトルバーで進捗状況を確認できます。(Y/N)");
+string? verboseCheck = Console.ReadLine();
+if (verboseCheck != null && verboseCheck == "Y" || verboseCheck == "y")
+    verboseMode = true;
+
+Console.WriteLine();
+
+List<string> pathList = new List<string>();
+List<string> crcList = new List<string>();
+List<string> fileNameList = new List<string>();
+if (path != null)
+{
+    pathList = GetTypeList(path, "path");
+    crcList = GetTypeList(path, "Crc");
+    fileNameList = GetTypeList(path, "fileName");
+}
+
+if(folderName != null && folderName != "") // 変換するフォルダが指定された場合、ここでそのフォルダ以外のものをリストから削除
+{
+    if (verboseMode)
+        Console.WriteLine("変換フォルダが指定されているため、リストの再構築を開始");
+
+    int ogCount = 0;
+    while(ogCount != pathList.Count)
+    {
+        if (!pathList[ogCount].StartsWith(folderName + "/"))
+        {
+            if (verboseMode)
+                Console.WriteLine("リストから削除: {0}", pathList[ogCount]);
+
+            pathList.RemoveAt(ogCount);
+            crcList.RemoveAt(ogCount);
+            fileNameList.RemoveAt(ogCount);
+        } else
+            ogCount++;
+    }
+}
+
+if (verboseMode)
+    Console.WriteLine("ファイルの変換を開始");
+
+for (int i = 0; i < pathList.Count; i++)
+{
+    string[] files = new string[] { };
+    if (path != null)
+    {
+        if (verboseMode)
+            Console.WriteLine("ソースファイルが存在するか確認: {0}", $"*_{crcList[i]}.dat");
+
+        files = Directory.GetFiles(path, $"*_{crcList[i]}.dat");
+    }
+    if (files.Length >= 1)
+    {
+        string copyPath = pathList[i];
+        string willBeCopiedPath = "";
+        if (destPath != null)
+            willBeCopiedPath = Path.Combine(destPath, copyPath);
+        string sourcePath = files[0].Substring(0, files[0].IndexOf("_"));
+        if (destPath != null)
+        {
+            string dir = Path.Combine(destPath, copyPath.Replace(fileNameList[i], ""));
+            if (!Directory.Exists(dir))
+            {
+                if (verboseMode)
+                    Console.WriteLine("ディレクトリ\"{0}\"は存在しないので作成します", dir);
+
+                Directory.CreateDirectory(dir);
+            }
+
+            if (File.Exists(willBeCopiedPath) && !FileCompare(willBeCopiedPath, sourcePath))
+            {
+                if (verboseMode)
+                    Console.WriteLine("ファイルは既に変換されていましたが、ソースファイルと異なっていたので、削除します。");
+
+                File.Delete(willBeCopiedPath);
+            }
+
+            if (!File.Exists(willBeCopiedPath))
+            {
+                if (verboseMode)
+                    Console.WriteLine("ファイルをコピー: {0}", willBeCopiedPath);
+
+                File.Copy(sourcePath, willBeCopiedPath, false);
+            }
+        }
+    }
+
+    UpdateConsoleTitle(Status.Copying, i, pathList.Count);
+}
+
+UpdateConsoleTitle(Status.Done);
+Console.WriteLine("完了しました。何かキーを押して終了します...");
+Console.Read();
---
+++ Blue-Archive-Assets-Converter/Utils.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blue_Archive_Assets_Converter
+{
+    internal class Utils
+    {
+        const string APP_NAME = "Blue Archive Assets Converter";
+        public static string mediaCatalogJson = "MediaCatalog.json";
+        public static bool verboseMode = false;
+        public static List<string> GetTypeList(string jsonPath, string type) // どうやってJsonを読み込めばいいのかわからなかったのでごり押しです 非効率的すぎワロタ
+        {
+            if (verboseMode)
+                Console.WriteLine("タイプ\"{0}\"のListの構築を開始", type);
+
+            string json = File.ReadAllText(Path.Combine(jsonPath, mediaCatalogJson));
+            string[] dataArray = json.Split(",");
+            string[] toBeRemovedStrArray = { "\\", "\"", ":", type };
+            List<string> processedList = new List<string>();
+            for(int i = 0; i <= dataArray.Length - 1; i++)
+            {
+                string targetString = dataArray[i];
+                if(targetString.Contains(type))
+                {
+                    foreach(string toBeRemovedStr in toBeRemovedStrArray)
+                    {
+                        targetString = targetString.Replace(toBeRemovedStr, "");
+                    }
+                    processedList.Add(targetString);
+
+                    if (verboseMode)
+                        Console.WriteLine(targetString);
+                }
+            }
+            return processedList;
+        }
+
+        public enum Status
+        {
+            Init,
+            Copying,
+            Done
+        }
+        public static void UpdateConsoleTitle(Status status, int arg1 = 0, int arg2 = 0)
+        {
+            string title;
+            switch(status)
+            {
+                case Status.Copying:
+                    title = APP_NAME + " - " + $"ファイルをコピー中 ({arg1} / {arg2})";
+                    break;
+
+                case Status.Done:
+                    title = APP_NAME + " - " + "完了";
+                    break;
+
+                default:
+                    title = APP_NAME;
+                    break;
+            }
+
+            Console.Title = title;
+        }
+
+        // FileCompare関数はMicrosoftさんの"Visual C# を使用してFile-Compare関数を作成する"からいただきました。
+
+        // This method accepts two strings the represent two files to
+        // compare. A return value of 0 indicates that the contents of the files
+        // are the same. A return value of any other value indicates that the
+        // files are not the same.
+        public static bool FileCompare(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            // Determine if the same file was referenced two times.
+            if (file1 == file2)
+            {
+                // Return true to indicate that the files are the same.
+                return true;
+            }
+
+            // Open the two files.
+            fs1 = new FileStream(file1, FileMode.Open);
+            fs2 = new FileStream(file2, FileMode.Open);
+
+            // Check the file sizes. If they are not the same, the files
+            // are not the same.
+            if (fs1.Length != fs2.Length)
+            {
+                // Close the file
+                fs1.Close();
+                fs2.Close();
+
+                // Return false to indicate files are different
+                return false;
+            }
+
+            // Read and compare a byte from each file until either a
+            // non-matching set of bytes is found or until the end of
+            // file1 is reached.
+            do
+            {
+                // Read one byte from each file.
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            // Close the files.
+            fs1.Close();
+            fs2.Close();
+
+            // Return the success of the comparison. "file1byte" is
+            // equal to "file2byte" at this point only if the files are
+            // the same.
+            return ((file1byte - file2byte) == 0);
+        }
+    }
+}
