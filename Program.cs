531e3c36c766712fff58926b67459f09fea77d37
endergreen12 <endergreenjapan@gmail.com>
2024-05-05 19:38:25 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Blue_Archive_Assets_Converter;
+using MemoryPack;
+using System.Reflection;
+
+Console.WriteLine("Blue Archive Assets Converter v{0} | Endergreen12", Assembly.GetExecutingAssembly().GetName().Version);
+
+string mediaPatchPath = "";
+
+if (args[0] != null && args[0] == "")
+{
+    mediaPatchPath = args[0];
+}
+
+if(!Directory.Exists(mediaPatchPath))
+{
+    Console.WriteLine("");
+}
