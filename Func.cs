0a18e27220706b25975d87466eb550c87d5a5ce1
endergreen12 <endergreenjapan@gmail.com>
2024-05-06 01:03:57 +0900

    Rewrite code

---
+++ Func.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blue_Archive_Assets_Converter
+{
+    internal class Func
+    {
+        public enum Message
+        {
+            PressAnyKeyToExit,
+            EnterFolderPath,
+            FolderDoesNotExist,
+            SpecifyBinary,
+            BinaryNotFound,
+            MediaCatalogLoading,
+            DirCreated,
+            CopyStart,
+            Done
+        }
+
+        public enum Language
+        {
+            English,
+            Japanese
+        }
+
+        public static void pressAnyKey(Language lang)
+        {
+            Console.WriteLine(getLocalizedString(Message.PressAnyKeyToExit, lang));
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        // Localization | ローカライゼーション
+
+        public static Language getUserLanguage()
+        {
+            string country = System.Globalization.CultureInfo.CurrentCulture.Name;
+            Language language = Language.English; // Default language is English | デフォルトの言語は英語
+            // Use switch statements to account for the possibility of adding languages later
+            // 後から言語を追加することも考慮してswitchステートメントを使う
+            switch (country)
+            {
+                case "ja-JP":
+                    language = Language.Japanese;
+                    break;
+            }
+
+            return language;
+        }
+
+        public static string getLocalizedString(Message message, Language language) // TODO: Find a better way to localize | よりよいローカライズの方法を見つける
+        {                                                                           // default in switch means English | switchのdefaultは英語
+            string locString = "";
+            switch(message)
+            {
+                case Message.PressAnyKeyToExit:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "何かキーを押して終了します...";
+                            break;
+
+                        default:
+                            locString = "Press any key to exit...";
+                            break;
+                    }
+                    break;
+
+                case Message.EnterFolderPath:
+                    switch(language)
+                    {
+                        case Language.Japanese:
+                            locString = "MediaPatchフォルダーのパスを入力してください\n(例:D:\\BlueArchive\\com.YostarJP.BlueArchive\\files\\MediaPatch)";
+                            break;
+
+                        default:
+                            locString = "Enter the path to the MediaPatch folder\n(e.g., D:\\BlueArchive\\com.YostarJP.BlueArchive\\files\\MediaPatch)";
+                            break;
+                    }
+                    break;
+
+                case Message.FolderDoesNotExist:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "指定されたディレクトリが存在しませんでした";
+                            break;
+
+                        default:
+                            locString = "The specified directory does not exist";
+                            break;
+                    }
+                    break;
+
+                case Message.SpecifyBinary:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "{0}が指定されたフォルダの中に見つかりませんでした\n利用可能な{0}をフルパスで(ファイル名も含めて)入力してください\n(例:D:\\MediaCatalog\\MediaCatalog(1.44.278134).bytes)";
+                            break;
+
+                        default:
+                            locString = "{0} was not found in the specified folder\nPlease specify the full path (include file name) to {0} manually available\n(e.g., D:\\MediaCatalog\\MediaCatalog(1.44.278134).bytes)";
+                            break;
+                    }
+                    break;
+
+                case Message.BinaryNotFound:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "指定されたファイルが見つかりませんでした";
+                            break;
+
+                        default:
+                            locString = "The specified file could not be found";
+                            break;
+                    }
+                    break;
+
+                case Message.MediaCatalogLoading:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "MediaCatalogを読み込んでいます...";
+                            break;
+
+                        default:
+                            locString = "Loading MediaCatalog...";
+                            break;
+                    }
+                    break;
+
+                case Message.DirCreated:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "{0}フォルダをカレントディレクトリに作成しました";
+                            break;
+
+                        default:
+                            locString = "{0} folder is created in the current directory";
+                            break;
+                    }
+                    break;
+
+                case Message.CopyStart:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "ファイルのコピーを開始します\n({0}フォルダにコピーされます)";
+                            break;
+
+                        default:
+                            locString = "output folder is created in the current directory\n(Files will be copied to the {0} folder)";
+                            break;
+                    }
+                    break;
+
+                case Message.Done:
+                    switch (language)
+                    {
+                        case Language.Japanese:
+                            locString = "完了しました\n{0}フォルダはこのプログラムが実行されたフォルダにあるはずです";
+                            break;
+
+                        default:
+                            locString = "Done\nThe {0} folder should be in the folder where this program was run";
+                            break;
+                    }
+                    break;
+            }
+
+            return locString;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,106 @@
 ﻿using Blue_Archive_Assets_Converter;
+using static Blue_Archive_Assets_Converter.Func;
 using MemoryPack;
 using System.Reflection;
 
 Console.WriteLine("Blue Archive Assets Converter v{0} | Endergreen12", Assembly.GetExecutingAssembly().GetName().Version);
+Console.WriteLine();
 
 string mediaPatchPath = "";
+Language language = getUserLanguage();
 
-if (args[0] != null && args[0] == "")
+
+/*                                                                         
+                            Preparation | 準備                           
+                                                                          */
+
+
+Console.WriteLine(getLocalizedString(Message.EnterFolderPath, language));
+string enteredPath = Console.ReadLine();
+mediaPatchPath = enteredPath;
+
+mediaPatchPath = mediaPatchPath.Replace("\"", ""); // Remove double quatation from path | パスからダブルクォーテーションを削除する
+if(!Directory.Exists(mediaPatchPath)) // Exit program if directory does not exist | ディレクトリが存在しなかったらプログラムを終了する
 {
-    mediaPatchPath = args[0];
+    Console.WriteLine(getLocalizedString(Message.FolderDoesNotExist, language));
+    pressAnyKey(language);
 }
 
-if(!Directory.Exists(mediaPatchPath))
-{
-    Console.WriteLine("");
+Console.WriteLine();
+
+string mediaCatalogBinName = "MediaCatalog.bytes";
+string catalogBinPath = Path.Combine(mediaPatchPath, mediaCatalogBinName);
+if(!File.Exists(catalogBinPath)) // If MediaCatalog.bytes is not in the specified folder, ask the user for the binary path
+{                                // 指定されたフォルダの中にMediaCatalog.bytesがない場合はユーザーにバイナリのパスを求める
+    Console.WriteLine(getLocalizedString(Message.SpecifyBinary, language), mediaCatalogBinName);
+    string specifiedBinPath = Console.ReadLine();
+    specifiedBinPath = specifiedBinPath.Replace("\"", "");
+
+    if (!File.Exists(specifiedBinPath))
+    {
+        Console.WriteLine(getLocalizedString(Message.BinaryNotFound, language), mediaCatalogBinName);
+        pressAnyKey(language);
+    }
+
+    catalogBinPath = specifiedBinPath;
 }
+
+Console.WriteLine();
+
+
+/*                                                                         
+                            Loading | ローディング                           
+                                                                          */
+
+
+Console.WriteLine(getLocalizedString(Message.MediaCatalogLoading, language));
+
+// Load MediaCatalog | MediaCatalogを読み込む
+byte[] catalogBin = File.ReadAllBytes(catalogBinPath);
+MediaCatalog mediaCatalog = MemoryPackSerializer.Deserialize<MediaCatalog>(catalogBin);
+
+string outputFolderName = "output";
+if(!Directory.Exists(outputFolderName))
+{
+    Directory.CreateDirectory(outputFolderName);
+    Console.WriteLine(getLocalizedString(Message.DirCreated, language), outputFolderName);
+}
+Directory.SetCurrentDirectory(outputFolderName);
+
+// Copy the files using the information in each stored Media
+// 格納されたそれぞれのMediaの情報を使いファイルをコピーする
+Console.WriteLine(getLocalizedString(Message.CopyStart, language), outputFolderName);
+var curPos = Console.GetCursorPosition();
+var lastLogLength = 0;
+foreach (KeyValuePair<string, Media> catalog in mediaCatalog.Table)
+{
+    Media media = catalog.Value;
+    string[] gotFiles = Directory.GetFiles(mediaPatchPath, "*_" + media.Crc.ToString());
+    // Since the file name contains Crc, use it to confirm its existence
+    // ファイル名にCrcを含んでいるのでそれを利用して存在を確認する
+    // TODO: Reveal the mysterious UInt64 numbers in front of the Crc | Crcの前についてる謎のUInt64の数字の正体を明かす
+    // This | こいつ -> 652901576978586_4235271580 <- This is Crc | これはCrc
+    if (gotFiles.Length > 0)
+    {
+        // Create directory | ディレクトリを作成
+        string[] newDirectoryArray = media.Path.Split('/');
+        Directory.CreateDirectory(String.Join("\\", newDirectoryArray.SkipLast(1)));
+        File.Copy(gotFiles[0], media.Path, true);
+
+        // Log output on the same line, but it covers the previous output, so delete the previous output
+        // 同じ行でログを出力するが前の出力にかぶさるので前の出力を消す
+        string log = gotFiles[0] + " -> " + media.Path;
+
+        if(lastLogLength > 0)
+            Console.Write(new string(' ', lastLogLength));
+
+        lastLogLength = log.Length;
+        Console.SetCursorPosition(curPos.Left, curPos.Top);
+        Console.Write(log);
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(getLocalizedString(Message.Done, language), outputFolderName);
+pressAnyKey(language);
