5d4f10fdbce0a02acc04fd3cb5c957215fd8edc2
Endergreen12 <endergreenjapan@gmail.com>
2024-08-01 16:22:10 +0900

    Integrate CopyFiles function Add help Add some checks

--- Blue-Archive-Assets-Converter/Program.cs
+++ Blue-Archive-Assets-Converter/Program.cs
@@ -1,36 +1,25 @@
 ﻿using MemoryPack;
 using Blue_Archive_Classes;
+using static Utils.Utils;
 using static Utils.Ask;
 
-var newLineStr = Environment.NewLine;
+Console.WriteLine("Blue Archive Assets Converter");
+Console.WriteLine();
+DisplayHelpIfArgIsHelp(args, "Usage: Blue-Archive-Assets-Converter [CatalogType] [Asset folder path] " +
+    "[Catalog binary path] [Output folder path]" + newLineStr + "Example: Blue-Archive-Assets-Converter MediaCatalog E:\\MediaPatch E:\\MediaPatch\\MediaCatalog.bytes .\\output");
+
 var specifiedCatalogType = CatalogType.MediaCatalog;
 var assetFolderPath = "";
 var catalogBinPath = "";
 var outputFolderPath = "";
 
 // Ask CatalogType
-if(args.Length == 0 || args.Length >= 1 && !Enum.TryParse(args[0], out specifiedCatalogType))
-{
-    while(true)
-    {
-        Console.WriteLine("Catalog List:");
-        foreach(var catalog in Enum.GetValues<CatalogType>())
-        {
-            Console.WriteLine((int)catalog + ": " + catalog); // "0: MediaCatalog"
-        }
-        Console.WriteLine();
-        var specifiedCatalogTypeStr = AskStringForUser("Please enter the Catalog you wish to convert" + newLineStr + "Please select from the Catalog List above" + newLineStr + "You can select by number, but you can also type the name as it is");
-        if (Enum.TryParse(specifiedCatalogTypeStr, out specifiedCatalogType) && (int)specifiedCatalogType < Enum.GetValues<CatalogType>().Length)
-            break;
+ParseOrAskENumValue<CatalogType>("Please enter the Catalog you wish to convert" + newLineStr + "Please select from the Catalog List above" + newLineStr + "You can select by number, but you can also type the name as it is",
+    ref specifiedCatalogType, 0, args);
 
-        Console.WriteLine("Parse to CatalogType failed" + newLineStr + "Please enter it again");
-        Console.WriteLine();
-    }
-}
-
-assetFolderPath = AskAndValidPath(false, "Enter the path of the asset folder", 1, args);
-catalogBinPath = AskAndValidPath(true, "Enter the path of the catalog binary", 2, args);
-outputFolderPath = AskAndValidPath(false, "Enter the path of the output destination", 3, args);
+assetFolderPath = ParseOrAskAndValidPath(false, "Enter the path of the asset folder", 1, args);
+catalogBinPath = ParseOrAskAndValidPath(true, "Enter the path of the catalog binary", 2, args);
+outputFolderPath = ParseOrAskAndValidPath(false, "Enter the path of the output destination", 3, args);
 
 Directory.SetCurrentDirectory(outputFolderPath);
 
@@ -39,54 +28,77 @@ Console.WriteLine("Start copying" + newLineStr + "Logs are not displayed to impr
 switch(specifiedCatalogType)
 {
     case CatalogType.MediaCatalog:
-        CopyFilesWithMediaCatalog(catalogBinPath);
+        CopyFilesWithCatalog(catalogBinPath, typeof(MediaCatalog));
         break;
 
     case CatalogType.TableCatalog:
-        CopyFilesWithTableCatalog(catalogBinPath);
+        CopyFilesWithCatalog(catalogBinPath, typeof(TableCatalog));
+        break;
+
+    default:
+        Console.WriteLine($"Error: The specified CatalogType {specifiedCatalogType} is invalid");
         break;
 }
 
 Console.WriteLine("Done" + newLineStr + "Press enter key to exit...");
 Console.ReadLine();
 
-void CopyFilesWithMediaCatalog(string catalogBinPath)
+void CopyFilesWithCatalog(string catalogBinPath, Type type)
 {
-    var catalog = MemoryPackSerializer.Deserialize<MediaCatalog>(File.ReadAllBytes(catalogBinPath));
-    if(catalog == null)
+    byte[]? catalogBin = null;
+    if(File.Exists(catalogBinPath))
     {
-        Console.WriteLine("Failed to load catalog");
+        catalogBin = File.ReadAllBytes(catalogBinPath);
+    } else
+    {
+        Console.WriteLine("Catalog binary file not found");
         return;
     }
-    foreach(var pair in catalog.Table)
+
+    if(catalogBin == null)
     {
-        var media = pair.Value;
-        var srcFileArray = Directory.GetFiles(assetFolderPath, "*_" + media.Crc.ToString());
-        if (srcFileArray.Length > 0)
+        Console.WriteLine("Failed to load catalog binary - variable is null");
+        return;
+    }
+
+    if (type == typeof(MediaCatalog))               // MediaCatalog copy process
+    {
+        var catalog = MemoryPackSerializer.Deserialize<MediaCatalog>(catalogBin);
+        if (catalog == null)
         {
-            var pathArray = media.Path.Split("/");
-            Directory.CreateDirectory(Path.Combine(pathArray.SkipLast(1).ToArray())); // Remove file name from path and create directories
-            File.Copy(srcFileArray[0], Path.Combine(pathArray));
+            Console.WriteLine("Failed to load catalog - variable is null");
+            return;
         }
-    }
-}
 
-void CopyFilesWithTableCatalog(string catalogBinPath)
-{
-    var catalog = MemoryPackSerializer.Deserialize<TableCatalog>(File.ReadAllBytes(catalogBinPath));
-    if (catalog == null)
-    {
-        Console.WriteLine("Failed to load catalog");
-        return;
-    }
-    foreach (var pair in catalog.Table)
-    {
-        var tableBundle = pair.Value;
-
-        var srcFileArray = Directory.GetFiles(assetFolderPath, "*_" + tableBundle.Crc.ToString());
-        if (srcFileArray.Length > 0)
+        foreach (var pair in catalog.Table)
         {
-            File.Copy(srcFileArray[0], tableBundle.Name);
+            var media = pair.Value;
+            var srcFileArray = Directory.GetFiles(assetFolderPath, "*_" + media.Crc.ToString());
+            if (srcFileArray.Length > 0)
+            {
+                var pathArray = media.Path.Split("/");
+                Directory.CreateDirectory(Path.Combine(pathArray.SkipLast(1).ToArray())); // Remove file name from path and create directories
+                File.Copy(srcFileArray[0], Path.Combine(pathArray));
+            }
+        }
+    } else if(type == typeof(TableCatalog))         // TableCatalog copy process
+    {
+        var catalog = MemoryPackSerializer.Deserialize<TableCatalog>(catalogBin);
+        if (catalog == null)
+        {
+            Console.WriteLine("Failed to load catalog - variable is null");
+            return;
+        }
+
+        foreach (var pair in catalog.Table)
+        {
+            var tableBundle = pair.Value;
+
+            var srcFileArray = Directory.GetFiles(assetFolderPath, "*_" + tableBundle.Crc.ToString());
+            if (srcFileArray.Length > 0)
+            {
+                File.Copy(srcFileArray[0], tableBundle.Name);
+            }
         }
     }
 }
